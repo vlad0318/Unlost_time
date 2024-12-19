67a8fb39c652a403cc4325bd3c54fe488d500acf
vlad0318 <revutskyyvlad@gmail.com>
2024-12-19 15:19:36 -0800

    made for testing

---
+++ main.py
@@ -0,0 +1,7 @@
+import memory
+
+i=memory.memory()
+
+i.add_event("code", "now", "high", "unspecified")
+i.add_event("code", "then", "high", "unspecified")
+i.add_event("work", "now", "high", "unspecified")
