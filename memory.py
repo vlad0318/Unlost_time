8ebb78b3fa83a2d66d74e06f554be4a81f691526
vlad0318 <revutskyyvlad@gmail.com>
2024-12-19 11:44:55 -0800

    created the memory script that handels json for the app also created pseoudo code --Vladyslav Revutskyy

---
+++ memory.py
@@ -0,0 +1,34 @@
+"""
+Program name: memory.py
+Written By: Vladyslav Revutskyy
+
+
+Tasks: 
+    manipulates user input for the main program
+
+High level pseudo code:
+    1. create a way to create the file 
+    2. create a function to clear the file
+    3. create a function to append to the file
+    4. create a function to feed the contet of the file to python dictionary
+    5. remove individual pieces from the file]
+
+This will be done using JSON files
+
+Each task will look like this:
+
+{
+    "tasks":
+    {
+        "task_name": "",
+        "time": "",
+        "improtance": "",
+        "location": "",
+    }
+"""
+
+import json
+
+class memory:
+    def __init__(self):
+        pass
