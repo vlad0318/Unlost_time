fbb6348a40a40baf01cbe8896c5a7fef7cea54c4
vlad0318 <revutskyyvlad@gmail.com>
2024-12-20 13:09:06 -0800

    rewrote the memeory.py file to get it the way that I like

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import memory
 
 i=memory.memory()
 
-i.add_event("code", "now", "low", "unspecified")
-i.add_event("code", "then", "high", "unspecified")
-i.add_event("work", "now", "high", "unspecified")
+i.add_task("code", "now", "low", "unspecified")
+i.add_task("code", "then", "high", "unspecified")
+i.add_task("work", "now", "high", "unspecified")
+
+i.write()
+
+x=i.read_json()
+
+print(x)
+
+i.remove_task("work")
+
+i.write()
